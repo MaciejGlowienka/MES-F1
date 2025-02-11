9efe83962777375b00d695ac367977555ac55838
MaciejGlowienka <maciek.glowienka@gmail.com>
2025-02-11 17:23:02 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MES_F1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
