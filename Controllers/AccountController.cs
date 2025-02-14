00d3f2b436ff3e6a9d5e3093ce73027f5922f889
MaciejGlowienka <maciek.glowienka@gmail.com>
2025-02-14 18:10:15 +0100

    ApplicationUserWorkingBase

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using MES_F1.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        return View(users);
+    }
+}
